e011ee182438abaa71dd129263955c599553c40c
Hamid Nikoofard <156426727+hamidnikoofard@users.noreply.github.com>
2025-10-21 18:33:53 +0330

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-console": "warn",      
+    },
+  },
+  
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
